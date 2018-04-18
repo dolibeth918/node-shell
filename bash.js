09d59a057d9522610c762d44d103df34ba821366
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-04-18 14:54:02 -0400

    completed up to handling asynchronicity

--- bash.js
+++ bash.js
@@ -2,19 +2,57 @@ let commands = require('./commands.js');
 
 process.stdout.write('prompt > ');
 
-//if the user says "pwd", then we'll print
+var done = function(output) {
+  process.stdout.write(output + '\n');
+  process.stdout.write('prompt > ');
+}
+
 process.stdin.on('data', function (data){
   let result = data.toString().trim();
-  if (result === 'pwd'){
-    commands.pwd();
+
+  let command = result;
+  let content;
+
+  for (let i = 0; i < result.length; i++){
+    if (result[i] === ' ') {
+      command = result.slice(0, i);
+      content = result.slice(i + 1);
+      break;
+    }
   }
-  if (result === 'date'){
+
+  if (command === 'curl'){
+    commands.curl(content, done);
+  }
+
+  if (command === 'tail'){
+    commands.tail(content, done);
+  }
+
+  if (command === 'head'){
+    commands.head(content, done);
+  }
+
+  if (command === 'cat'){
+    commands.cat(content, done);
+  }
+
+  if (command === 'echo'){
+    commands.echo(content, done);
+  }
+
+  if (command === 'pwd'){
+    commands.pwd(done);
+  }
+
+  if (command === 'date'){
     let date = new Date().toString();
     process.stdout.write(date + '\n');
     process.stdout.write('prompt > ');
   }
-  if (result === 'ls'){
-    commands.ls();
+
+  if (command === 'ls'){
+    commands.ls(done);
   }
 });
 
--- commands.js
+++ commands.js
@@ -1,17 +1,55 @@
 var fs = require('fs');
+let request = require('request');
 
-var filepath = process.argv[1].toString().slice(0, -8);
+var filepath = __dirname;
 
 module.exports = {
-  pwd: function(){
-    process.stdout.write(filepath + '\n');
-    process.stdout.write('prompt > ');
+  pwd: function(done){
+    let result = filepath;
+    done(result);
   },
-  ls: function(){
+  ls: function(done){
     fs.readdir(filepath, function(err, files){
       if (err) throw err;
-        files.forEach(file => console.log(file));
-        process.stdout.write('prompt > ');
+      let result = '';
+      for (let i = 0; i < files.length; i++){
+        result += files[i] + '     ';
+      }
+      done(result);
     })
+  },
+  //need to write a function to process echo!
+  echo: function(content, done) {
+    let result = content + '\n';
+    done(result);
+  },
+  cat: function(filename, done) {
+    fs.readFile(filename, function(err, data){
+      if (err) throw err;
+      data = data.toString();
+      done(data);
+    });
+  },
+  head: function(filename, done) {
+    let path = `./${filename}`;
+    fs.readFile(path, 'utf8', function(err, data){
+      if (err) throw err;
+      let result = data.split('\n').slice(0, 5).join('\n')
+      done(result);
+    })
+  },
+  tail: function(filename, done) {
+    let path = `./${filename}`;
+    fs.readFile(path, 'utf8', function(err, data){
+      if (err) throw err;
+      let result = data.split('\n').slice(-5).join('\n');
+      done(result);
+    })
+  },
+  curl: function(url, done) {
+    request(url, function(err, response, body){
+      if (err) throw err;
+      done(body);
+    });
   }
 }
